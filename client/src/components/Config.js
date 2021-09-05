87d217580e4ecfaea213b81f8c09123e0045ab8f
duck <godqhr2256@gmail.com>
2021-09-05 18:46:02 +0900

    landing Page 수정

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,5 +1,5 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
-
+export const API_KEY = '586c54411c347f5699d01264369dfd5e';
 export const API_URL = 'https://api.themoviedb.org/3/';
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
